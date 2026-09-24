e468b7d8810ee64e7025d7ac20f8a64f71245102
Gustavo <gustavo.mmlima@gmail.com>
2026-09-23 23:46:08 -0300

    configurable timeouts

--- src/main/java/com/checkout/payment/gateway/configuration/ApplicationConfiguration.java
+++ src/main/java/com/checkout/payment/gateway/configuration/ApplicationConfiguration.java
@@ -1,6 +1,7 @@
 package com.checkout.payment.gateway.configuration;
 
 import java.time.Duration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,10 +11,13 @@ import org.springframework.web.client.RestTemplate;
 public class ApplicationConfiguration {
 
   @Bean
-  public RestTemplate restTemplate(RestTemplateBuilder builder) {
+  public RestTemplate restTemplate(
+      RestTemplateBuilder builder,
+      @Value("${acquiring-bank.connect-timeout-ms}") long connectTimeoutMs,
+      @Value("${acquiring-bank.read-timeout-ms}") long readTimeoutMs) {
     return builder
-        .setConnectTimeout(Duration.ofMillis(10000))
-        .setReadTimeout(Duration.ofMillis(10000))
+        .setConnectTimeout(Duration.ofMillis(connectTimeoutMs))
+        .setReadTimeout(Duration.ofMillis(readTimeoutMs))
         .build();
   }
 }
